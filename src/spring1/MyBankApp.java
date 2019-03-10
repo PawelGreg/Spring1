04e5f9dc068b4af2745c348b6805792f00febdba
PawelGreg <pawel.g.flis@gmail.com>
2019-03-11 00:31:39 +0100

    Add ATMCard. Define dependency injection.

--- src/spring1/MyBankApp.java
+++ src/spring1/MyBankApp.java
@@ -4,9 +4,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyBankApp {
     public static void main(String[] args) {
-        //Account account = new CurrentAccount();
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Account account = context.getBean("myAccount",Account.class);
         System.out.println(account.createAccount());
+        System.out.println(account.cardDetails());
     }
 }
--- src/spring1/model/CurrentAccount.java
+++ src/spring1/model/CurrentAccount.java
@@ -3,8 +3,14 @@ package spring1.model;
 import spring1.Account;
 
 public class CurrentAccount implements Account {
+
     @Override
     public String createAccount() {
         return "Current Account created succesfully";
+    }
+
+    @Override
+    public String cardDetails() {
+        return null;
     }
 }
